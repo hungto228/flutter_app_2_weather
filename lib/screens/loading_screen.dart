b3d7fc8b771b2d2b7ac0e3c476023c5eda9da14b
hungto228@gmail.com <hungto228@gmail.com>
2021-07-20 20:37:44 +0700

    get data api

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_app_2_weather/services/location.dart';
-import 'package:geolocator/geolocator.dart';
+import 'package:http/http.dart';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -12,6 +12,7 @@ class _LoadingScreenState extends State<LoadingScreen> {
   void initState() {
     super.initState();
     getLocation();
+    //   getData();
     print("hii");
   }
 
@@ -23,6 +24,18 @@ class _LoadingScreenState extends State<LoadingScreen> {
     print(" location${location.latitude}");
   }
 
+  //getdata
+  void getData() async {
+    Response response = await get(Uri.parse(
+        "http://api.openweathermap.org/data/2.5/weather?q=London,Uk&APPID=a35293af4d5a42fb921c6d89103c4caf"));
+    if (response.statusCode == 200) {
+      String data = response.body;
+      print(data);
+    } else {
+      print(response.statusCode);
+    }
+  }
+
   void somethingThatExpectsLesstThan10(int n) {
     if (n > 10) {
       throw "  n is greater than 10, n should alway be less than 10";
@@ -31,19 +44,21 @@ class _LoadingScreenState extends State<LoadingScreen> {
 
   @override
   Widget build(BuildContext context) {
-    String myMagin = "5s";
-    dynamic myMaginAsDouble;
-    try {
-      myMaginAsDouble = double.parse(myMagin);
-    } catch (e) {
-      print(e);
-      //myMaginAsDouble = 30.0;
-    }
-    return Scaffold(
-      body: Container(
-        margin: EdgeInsets.all(myMaginAsDouble ?? 30),
-        color: Colors.red,
-      ),
-    );
+    getData();
+    return Scaffold();
+    // String myMagin = "5s";
+    // dynamic myMaginAsDouble;
+    // try {
+    //   myMaginAsDouble = double.parse(myMagin);
+    // } catch (e) {
+    //   print(e);
+    //   //myMaginAsDouble = 30.0;
+    // }
+    // return Scaffold(
+    //   body: Container(
+    //     margin: EdgeInsets.all(myMaginAsDouble ?? 30),
+    //     color: Colors.red,
+    //   ),
+    // );
   }
 }
